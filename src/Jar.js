97f4dd568baf7b6d4810b713e0be6c9520a83c66
Eric McCarthy <eric@limulus.net>
2014-02-02 10:45:54 -0700

    Manifest file parser.

--- src/Jar.js
+++ src/Jar.js
@@ -15,3 +15,58 @@ var Jar = module.exports = function (jarPath) {
 }
 inherits(Jar, EventEmitter)
 
+
+/**
+ * @private
+ * @param {string} manifest
+ * @reutrn {Object.<string,string>}
+ */
+Jar._parseManifest = function (manifest) {
+    var result = {"main": {}, "sections": {}}
+
+    var expectingSectionStart = false
+      , currentSection = null
+
+    manifest.split(/(?:\r\n|\r|\n)/).forEach(function (line, i) {
+        // Watch for blank lines, they mean we're starting a new section
+        if (line === "") {
+            expectingSectionStart = true
+            return
+        }
+
+        // Extract the name and value from entry line
+        var pair = line.match(/^([a-z_-]+): (.+)$/i)
+        if (!pair) {
+            _throwManifestParseError("expected a valid entry", i, line)
+        }
+        var name = pair[1], val = pair[2]
+
+        // Handle section start
+        if (expectingSectionStart && name !== "Name") {
+            _throwManifestParseError("expected section name", i, line)
+        }
+        else if (expectingSectionStart) {
+            currentSection = val
+            expectingSectionStart = false
+            return
+        }
+
+        // Add entry to the appropriate section
+        if (currentSection) {
+            if (!result["sections"][currentSection]) {
+                result["sections"][currentSection] = {}
+            }
+            result["sections"][currentSection][name] = val
+        }
+        else {
+            result["main"][name] = val
+        }
+    })
+
+    return result
+}
+
+
+var _throwManifestParseError = function (message, lineNumber, lineValue) {
+    throw new Error("Error parsing manifest file, " + message + ", on line " + lineNumber + ": " + lineValue)
+}
--- test/Jar_test.js
+++ test/Jar_test.js
@@ -1,7 +1,28 @@
 "use strict"
 
 var Jar = require("../src/Jar.js")
+  , assert = require("assert")
 
 describe("Jar", function () {
+    describe("_parseManifest", function () {
+        it("should throw an error when it hits an invalid line", function () {
+            assert.throws(function () {
+                Jar._parseManifest("bogus")
+            })
+        })
 
+        it("should throw an error when it hits an invalid section start", function () {
+            assert.throws(function () {
+                Jar._parseManifest("Manifest-Version: 1.0\n\nInvalid-Section-Start: foo\nBar: baz")
+            })
+        })
+
+        it("should return an object with the manifest's key-values", function () {
+            var mf = Jar._parseManifest(manifestContents)
+            assert.strictEqual(mf["main"]["Manifest-Version"], "1.0")
+            assert.strictEqual(mf["sections"]["foo"]["Bar"], "baz")
+        })
+    })
 })
+
+var manifestContents = "Manifest-Version: 1.0\r\nCreated-By: 1.6.0_65 (Apple Inc.)\r\nMain-Class: net.desert.hello.Hello\r\n\r\nName: foo\r\nBar: baz\r\n"
